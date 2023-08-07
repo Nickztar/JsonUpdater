761c500a1906c20e344d78c2440d99829c15834b
Nicholas Brostrom <nicholas.brostrom@effectsoft.se>
2023-08-07 16:25:00 +0200

    Initial commit

---
+++ models/JiraProjectModel.cs
@@ -0,0 +1,85 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Effectsoft.Atlassian.Jira.Models
+{
+    public class JiraProjectModel
+    {
+        // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+        public class AvatarUrls
+        {
+            [JsonProperty("48x48")]
+            public string? _48x48 { get; set; }
+
+            [JsonProperty("24x24")]
+            public string? _24x24 { get; set; }
+
+            [JsonProperty("16x16")]
+            public string? _16x16 { get; set; }
+
+            [JsonProperty("32x32")]
+            public string? _32x32 { get; set; }
+        }
+
+        public class Insight
+        {
+            public int totalIssueCount { get; set; }
+            public DateTime lastIssueUpdateTime { get; set; }
+        }
+
+        public class ProjectCategory
+        {
+            public string? self { get; set; }
+            public string? id { get; set; }
+            public string? name { get; set; }
+            public string? description { get; set; }
+        }
+
+        public class Root
+        {
+            public string? self { get; set; }
+            public string? nextPage { get; set; }
+            public int maxResults { get; set; }
+            public int startAt { get; set; }
+            public int total { get; set; }
+            public bool isLast { get; set; }
+            public List<Value> values { get; set; }
+        }
+
+        public class Value
+        {
+            public string? self { get; set; }
+            public string? id { get; set; }
+            public string? key { get; set; }
+            public string? name { get; set; }
+            public AvatarUrls avatarUrls { get; set; }
+            public ProjectCategory projectCategory { get; set; }
+            public bool simplified { get; set; }
+            public string? style { get; set; }
+            public Insight insight { get; set; }
+        }
+
+
+        public class Version
+        {
+            public string? self { get; set; }
+            public string? id { get; set; }
+            public string? description { get; set; }
+            public string? name { get; set; }
+            public bool archived { get; set; }
+            public bool released { get; set; }
+            public string? releaseDate { get; set; }
+            public bool overdue { get; set; }
+            public string? userReleaseDate { get; set; }
+            public string? projectId { get; set; }
+        }
+
+
+
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,81 @@
+use std::fs::{self, File};
+use std::io::{prelude::*, BufReader, BufWriter, LineWriter};
+use std::path::PathBuf;
+
+fn main() -> anyhow::Result<()> {
+    println!("Hello, world!");
+    let models = fs::read_dir("models/")?;
+    for file in models.into_iter().filter(|c| {
+        c.as_ref().is_ok_and(|n| {
+            n.file_name()
+                .to_str()
+                .is_some_and(|df| !df.starts_with("new_"))
+        })
+    }) {
+        let file = file?;
+        let file_name = file.file_name();
+        let model = File::open(file.path())?;
+        let reader = BufReader::new(model);
+        let mut new_file = File::create("models/new_".to_owned() + file_name.to_str().unwrap())?;
+        let mut prev_line = None;
+        for line in reader.lines() {
+            let line = line?;
+            if !line.contains("get;") {
+                new_file.write(line.as_bytes())?;
+                new_file.write(&[b'\n'])?;
+                prev_line = Some(line);
+                continue;
+            }
+
+            if let Some(identifier) = line.split_whitespace().nth(2) {
+                let new_line =
+                    line.replace(identifier, make_ascii_titlecase(&mut identifier.to_owned()));
+                println!("new line? {}, ident: {}", new_line, identifier);
+                println!("prev line? {:?}", prev_line);
+                if prev_line
+                    .clone()
+                    .and_then(|prev| {
+                        if prev.contains("JsonProperty") {
+                            Some(prev)
+                        } else {
+                            None
+                        }
+                    })
+                    .is_none()
+                {
+                    let mut whitespace = 0;
+                    let chars = line.chars();
+                    for char in chars {
+                        if char::is_whitespace(char) {
+                            whitespace += 1
+                        } else {
+                            break;
+                        }
+                    }
+                    new_file.write(
+                        format!(
+                            "{:indent$}JsonProperty[(\"{}\")]",
+                            "",
+                            identifier,
+                            indent = whitespace,
+                        )
+                        .as_bytes(),
+                    )?;
+                    new_file.write(&[b'\n'])?;
+                }
+                new_file.write(new_line.as_bytes())?;
+                new_file.write(&[b'\n'])?;
+            }
+        }
+        new_file.flush()?;
+    }
+
+    Ok(())
+}
+
+fn make_ascii_titlecase(s: &mut str) -> &str {
+    if let Some(r) = s.get_mut(0..1) {
+        r.make_ascii_uppercase();
+    }
+    s
+}
